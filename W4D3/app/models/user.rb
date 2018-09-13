56b26a4bd58c23fa324e289cb018ec3f5e91fe0e
ajgosling <ajgosling2@gmail.com>
2018-09-12 17:21:54 -0700

    Add functionality for users to own and lease kitties

--- W4D3/app/models/user.rb
+++ W4D3/app/models/user.rb
@@ -16,6 +16,10 @@ class User < ApplicationRecord
   
   attr_reader :password
   
+  has_many :cats,
+    foreign_key: :user_id,
+    class_name: :Cat
+  
   def reset_session_token!
     self.session_token = SecureRandom.urlsafe_base64
     self.save
